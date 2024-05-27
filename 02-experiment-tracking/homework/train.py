596c24412cbe31b01c7bfcf9248fcbd3a8c0113b
FiLM <keerathap@gmail.com>
2024-05-28 01:15:28 +0700

    finish hw 02-experiment-tracking

--- 02-experiment-tracking/homework/train.py
+++ 02-experiment-tracking/homework/train.py
@@ -1,9 +1,14 @@
 import os
 import pickle
 import click
-
+import math
+import mlflow
+from mlflow.models import infer_signature
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_experiment("nyc-taxi-experiment")
 
 
 def load_pickle(filename: str):
@@ -15,19 +20,36 @@ def load_pickle(filename: str):
 @click.option(
     "--data_path",
     default="./output",
-    help="Location where the processed NYC taxi trip data was saved"
+    help="Location where the processed NYC taxi trip data was saved",
 )
 def run_train(data_path: str):
+    with mlflow.start_run():
+        mlflow.set_tag("developer", "FiLM")
+        mlflow.log_param("train-data-path", os.path.join(data_path, "train.pkl"))
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        mlflow.log_param("valid-data-path", os.path.join(data_path, "val.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+        params = rf.get_params()
+        mlflow.log_params(params)
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+        # Infer the model signature
+        signature = infer_signature(X_train, rf.predict(X_train))
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rmse = math.sqrt(mean_squared_error(y_val, y_pred))
+        mlflow.log_metric("rmse", rmse)
+        # Log the model
+        model_info = mlflow.sklearn.log_model(
+            sk_model=rf,
+            artifact_path="artifacts",
+            signature=signature,
+            input_example=X_train,
+            registered_model_name="tracking-quickstart",
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_train()
---
+++ 02-experiment-tracking/output/README.md
@@ -0,0 +1,15 @@
+After execute this command:
+
+```bash
+python preprocess_data.py --raw_data_path <TAXI_DATA_FOLDER> --dest_path ./output
+```
+
+We'll get
+
+```
+./ouput
+    |_ dv.pkl
+    |_ test.pkl
+    |_ train.pkl
+    |_ val.pkl
+```
